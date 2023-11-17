15fb605d12518cb33ea510d5d4c86fbb787b8c73
kingo555 <107342259+kingo555@users.noreply.github.com>
2023-11-17 14:56:54 +0900

    [Bronze II] Title: 공 바꾸기, Time: 244 ms, Memory: 18504 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int M=sc.nextInt();
+		int temp;
+		int[] arr=new int[N];
+		
+		for (int i=0; i<N; i++)
+		{
+			arr[i]=i+1;
+		}
+		for(int i=0; i<M; i++)
+		{
+			int a=sc.nextInt()-1;
+			int b=sc.nextInt()-1;
+			temp=arr[a];
+			arr[a]=arr[b];
+			arr[b]=temp;
+		}
+		for(int i=0; i<N; i++) {
+		System.out.print(arr[i]+" ");
+		}
+	}
+}
