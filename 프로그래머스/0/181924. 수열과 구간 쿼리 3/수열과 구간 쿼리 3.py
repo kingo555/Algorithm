5b75c598e9247751b832479f6ea600bdf5409b27
kingo555 <107342259+kingo555@users.noreply.github.com>
2024-04-20 16:16:09 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.22 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -0,0 +1,8 @@
+def solution(arr, queries):
+    temp = 0
+    for i in range(len(queries)) :
+        temp=arr[queries[i][0]]
+        arr[queries[i][0]] = arr[queries[i][1]]
+        arr[queries[i][1]] = temp
+    return arr
+        
