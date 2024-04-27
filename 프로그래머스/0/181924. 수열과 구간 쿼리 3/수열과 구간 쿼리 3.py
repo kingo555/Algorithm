8cfa804ee6b150cdbd80dfd312e52474298989c1
kingo555 <107342259+kingo555@users.noreply.github.com>
2024-04-27 22:34:12 +0900

    Update 수열과 구간 쿼리 3.py

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -3,6 +3,9 @@ def solution(arr, queries):
     for i in range(len(queries)) :
         temp=arr[queries[i][0]]
         arr[queries[i][0]] = arr[queries[i][1]]
+        
         arr[queries[i][1]] = temp
+
+    
     return arr
         
