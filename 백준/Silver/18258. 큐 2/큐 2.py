6fc548a62937c144b8fe2510d61f8a1380032a7a
김광오 <107342259+kingo555@users.noreply.github.com>
2024-10-22 17:23:09 +0900

    [Silver IV] Title: 큐 2, Time: 2376 ms, Memory: 110564 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline().rstrip())
+q = deque()
+
+for _ in range(n) :
+    a = list(map(str, sys.stdin.readline().rstrip().split()))
+
+    if a[0] =='push' :
+        q.append(int(a[1]))
+    elif a[0] =='pop' :
+        if len(q) == 0 :
+            print(-1)
+        else :
+            print(q.popleft())
+    elif a[0] == 'size' :
+        print(len(q))
+    elif a[0] == 'empty' :
+        if len(q) == 0 :
+            print(1)
+        else :
+            print(0)
+    elif a[0] == 'front' :
+        if len(q) == 0 :
+            print(-1)
+        else :
+            print(q[0])
+    elif a[0] == 'back' :
+        if len(q) == 0 :
+            print(-1)
+        else :
+            print(q[-1])
